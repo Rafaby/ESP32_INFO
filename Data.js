1be733cad023fc1f29771e35510b3414cafbab63
Rafaby <fabriceheriniaina74@gmail.com>
2024-06-18 19:49:54 +0300

    Data.js

--- Data.js
+++ Data.js
@@ -1,1 +1,11 @@
 
+var sensorData = {
+    temperature1: 20,
+    temperature2: 10,
+    Humidity1: 75,
+    Humidity2: 79,
+    relay1: false,
+    relay2: false,
+    relay3: false,
+    relay4: false
+  };
